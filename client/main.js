ab65648b6fbe3424acc9a5d9b8e9e0bb611d2f33
Brian Williams <heffergod@gmail.com>
2022-09-21 13:22:43 -0700

    page ready to deploy and go live

--- client/main.js
+++ client/main.js
@@ -1,1 +1,2 @@
 console.log('Hello, mofo')
+axios.post('/register')
--- server/index.js
+++ server/index.js
@@ -7,13 +7,16 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/index.html'))
-})
+app.use('/', express.static(path.join(__dirname, '../client/index.html')))
+app.use(express.static(path.join(__dirname, '../client')))
 
-app.get('/js', (req,res) => {
-    res.sendFile(path.join(__dirname, '../client/main.js'))
-})
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/index.html'))
+// })
+
+// app.get('/js', (req,res) => {
+//     res.sendFile(path.join(__dirname, '../client/main.js'))
+// })
 
 app.listen(port, ()=> {
     console.log('Docked at port 4000')
